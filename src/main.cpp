edd9d9ac469647b6be397e37def1c83ebf23d711
jjbboox <jjbboox@gmail.com>
2020-08-21 23:50:46 +0800

    LED灯指示

--- src/main.cpp
+++ src/main.cpp
@@ -7,24 +7,29 @@ GpioButton Btn1(BUTTON_1_PIN);
 
 void btn_1_click_event() {
 	Serial.println("<Event>Click");
+	digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
 }
 
 void btn_1_db_click_event() {
 	Serial.println("<Event>Double Click");
+	digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
 }
 
 void btn_1_long_press_event() {
 	Serial.println("<Event>Long Press Tick");
+	digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
 }
 
 void btn_1_long_click_event() {
 	Serial.println("<Event>Long Click");
+	digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
 }
 
 void setup() {
 	// 初始化串口用于输出调试信息
     Serial.begin(115200);
 	Serial.println("Start");
+	pinMode(LED_BUILTIN, OUTPUT);
 	
 	// 绑定事件回调函数
 	Btn1.bindEventOnClick(btn_1_click_event);
